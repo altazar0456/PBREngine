a03c67eb8c4d2b08624dc469924eacba8fd3340f
Oleg Chepurko <oleg0456@gmail.com>
2020-07-06 01:52:45 +0700

    - Added Vulkan Includes and Lib Paths - Wrote Base code for app

--- PBREngine/PBREngine.cpp
+++ PBREngine/PBREngine.cpp
@@ -1,39 +1,70 @@
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vec4.hpp>
 
+#include <iostream>
+
+const uint32_t kWindowWidth = 800;
+const uint32_t kWindowHeight = 600;
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        glfwInit();
+
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+    void initWindow()
+    {
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_window = glfwCreateWindow(kWindowWidth, kWindowHeight, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+        
+    }
+
+    void mainLoop()
+    {
+        while(!glfwWindowShouldClose(m_window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(m_window);
+
+        glfwTerminate();
+    }
+
+private:
+    GLFWwindow* m_window;
+};
+
 int main(void)
 {
-    GLFWwindow* window;
+    HelloTriangleApplication app;
 
-    /* Initialize the library */
-    if(!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if(!window)
+    try
     {
-        glfwTerminate();
-        return -1;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while(!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
-    return 0;
+    return EXIT_SUCCESS;
 }
